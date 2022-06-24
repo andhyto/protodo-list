85919f5370c0fe506b6d66b6412ecb64cd98caf8
Tom Hayes <tomhayespro@gmail.com>
2022-06-24 22:53:15 +0700

    Updates

---
+++ precache-manifest.91d29cee71e2a8572762c0c97ad762d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346290fb07c43d9d9d75",
+    "url": "/protodo-list/static/css/main.598acd11.chunk.css"
+  },
+  {
+    "revision": "346290fb07c43d9d9d75",
+    "url": "/protodo-list/static/js/main.f3abba0c.chunk.js"
+  },
+  {
+    "revision": "7c4c9dd881d0a7f380ec",
+    "url": "/protodo-list/static/js/runtime~main.bd79eb42.js"
+  },
+  {
+    "revision": "39d4179eb90d2d5f1b2a",
+    "url": "/protodo-list/static/js/2.3afcdef4.chunk.js"
+  },
+  {
+    "revision": "723c083e1577b98e0dba786c0bf3e8a6",
+    "url": "/protodo-list/index.html"
+  }
+];
